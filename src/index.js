a7e172d7cb6809c47d382a87be5dfade95820254
0xOmarA <OmarAbdulla7@hotmail.com>
2022-06-02 12:09:58 +0300

    Minor changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import App from './App';
+import './index.css';
 
 // Used for fonts
 import "@fontsource/ibm-plex-mono";
